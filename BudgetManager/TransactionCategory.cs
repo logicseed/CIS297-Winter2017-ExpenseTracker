4d3ba4323526a588a1fb0ee192684370597e91b5
Marc King <mjking@umich.edu>
2017-03-26 20:52:30 -0400

    Added account types and transaction categories.

---
+++ BudgetManager/TransactionCategory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetManager
+{
+    public enum TransactionCategory
+    {
+        Uncategorized,
+        Savings,
+        Groceries,
+        AdultClothing,
+        ChildrenClothing,
+        AutoPayment,
+        AutoInsurance,
+        AutoFuel,
+        AutoRepair,
+        PropertyMortgage,
+        PropertyRent,
+        PropertyTax,
+        PropertyRepair,
+        PropertyInsurance,
+        Electricity,
+        NaturalGas,
+        Water,
+        Trash,
+        Phone,
+        Internet,
+        Cable,
+        HealthInsurance,
+        ChildCare,
+        Entertainment,
+        LifeInsurance
+    }
+}
